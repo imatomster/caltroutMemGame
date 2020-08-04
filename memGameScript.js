54969706c9d9525d8ec7bc7553c89a2c0a7aba42
Tommy <imatomster@gmail.com>
2020-08-03 22:29:56 -0700

    starting matching

--- memGameScript.js
+++ memGameScript.js
@@ -1,16 +1,38 @@
 /* Javascript of the Memory Game */
 /* Global Variables */
-var cardArr = [];
-cardArr = document.querySelectorAll(".card");
-
+var cardArr = document.querySelectorAll(".card");
+var cardClicked = false;
+var firstCard;
+var secondCard;
 
 // Flipping Card Script
-window.onload = function(){
-	for(var i = 0; i < cardArr.length; i++){
-		cardArr[i].addEventListener("click", 
-			function(){
-				this.classList.toggle("flipCard");
-			}
-		, false);
+function playFlipSound(){
+	let flipSound = new Audio("audio/flip_sound.mp3");
+	flipSound.play();
+	flipSound.volume = 0.2;
+};
+
+function flipOverCard(){
+	this.classList.toggle("flip");
+	playFlipSound();
+
+	if(cardClicked == false){
+		cardClicked = true;
+		firstCard = this;
+		return ;
+	}
+
+	if(cardClicked == true){
+		secondCard = this;
+		isSame();
 	}
 };
+
+function isSame(){
+	// if(document.getElementById("myImg").alt)
+}
+
+for(let i = 0; i < cardArr.length; i++){
+	cardArr[i].addEventListener("click", flipOverCard, false);
+}
+
